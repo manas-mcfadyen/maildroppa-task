816b793dd3da0b19ac573e9827f3727e3bc9dd3d
manas-mcfadyen <manazharma@gmail.com>
2024-05-28 21:26:41 +0530

    feat: add hamburger in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,45 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Tab from "./common/Tabs";
 import useDevice from "@/utils/hooks/useDevice";
 
 const Header = () => {
-  const { isMobile } = useDevice();
+  const [showModal, setShowmodal] = useState(false);
 
   return (
-    <nav className="p-5 mb-10 md:mb-0 md:py-6 md:px-14 md:mx-10 flex justify-between items-center">
-      {/* Logo */}
-      <a href="/">
-        <Image src="/mail.svg" width={240} height={60} alt="Logo"></Image>
-      </a>
-      <div className="hidden  md:flex  md:gap-10">
-        <Tab href="/about">About Us</Tab>
-        <Tab href="/contact">Contact Us</Tab>
-        <Tab href="/pricing">Pricing</Tab>
-        <Tab href="/academy">Academy</Tab>
-      </div>
-    </nav>
+    <>
+      <nav className="p-5 mb-10 md:mb-0 md:py-6 md:px-14 md:mx-10 flex justify-between items-center">
+        {/* Logo */}
+        <a href="/">
+          <Image src="/mail.svg" width={240} height={60} alt="Logo"></Image>
+        </a>
+        <Image
+          onClick={() => setShowmodal(!showModal)}
+          src="/hamburger.svg"
+          width={30}
+          height={30}
+          alt="Logo"
+        ></Image>
+
+        <div className="hidden  md:flex  md:gap-10">
+          <Tab href="/about">About Us</Tab>
+          <Tab href="/contact">Contact Us</Tab>
+          <Tab href="https://maildroppa.com/pricing">Pricing</Tab>
+          <Tab href="https://maildroppa.com/academy">Academy</Tab>
+        </div>
+      </nav>
+      {showModal && (
+        <div
+          onClick={() => setShowmodal(!showModal)}
+          className="bg-white flex flex-col w-full text-black  p-5 text-md right-[-4.5rem] border-[1px] border-gray-400 rounded-md"
+        >
+          <Tab href="/about">About Us</Tab>
+          <Tab href="/contact">Contact Us</Tab>
+          <Tab href="https://maildroppa.com/pricing">Pricing</Tab>
+          <Tab href="https://maildroppa.com/academy">Academy</Tab>
+        </div>
+      )}
+    </>
   );
 };
 
